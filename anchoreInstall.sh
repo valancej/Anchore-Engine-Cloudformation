41b093b7b7887112797780de547d6c352ca2ce2f
Jeremy Valance <jvalance@Jeremys-MacBook-Pro.local>
2018-08-17 12:08:34 -0400

    Added docker-compose file

--- anchoreInstall.sh
+++ anchoreInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Install Docker and Docker Compose
 apt-get -y update;
 
 apt-get -y install \
@@ -19,6 +20,8 @@ apt-get -y update;
 apt-get -y install docker-ce;
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose;
 chmod +x /usr/local/bin/docker-compose;
+
+# Install Anchore Engine
 mkdir aevolume;
 cd aevolume;
 mkdir config && mkdir db
